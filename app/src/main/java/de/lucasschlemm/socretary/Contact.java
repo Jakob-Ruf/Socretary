4a31f64acbde460a3e211d5ba4e38a98230f240a
Lucas_Schlemm <Lucas@Schlemms.de>
2015-03-04 22:59:46 +0100

    Contact-Klasse erstellt. Befüllung von ID und Namen. Auswahl der Telefonnummer fehlt noch.

---
+++ app/src/main/java/de/lucasschlemm/socretary/Contact.java
@@ -0,0 +1,41 @@
+package de.lucasschlemm.socretary;
+
+/**
+ * Created by lucas.schlemm on 04.03.2015.
+ */
+public class Contact
+{
+    private int id;
+    private String name;
+    private String number;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+}
--- app/src/main/java/de/lucasschlemm/socretary/MainFragment.java
+++ app/src/main/java/de/lucasschlemm/socretary/MainFragment.java
@@ -1,6 +1,10 @@
 package de.lucasschlemm.socretary;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
@@ -9,7 +13,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * Created by lucas.schlemm on 04.03.2015.
@@ -28,22 +31,67 @@ public class MainFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
-        Button btnContact = (Button) v.findViewById(R.id.btn);
-        View.OnClickListener btnCL = new View.OnClickListener()
+        (v.findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                // Lädt den Intent mit dem ContactPicker
-                // TODO Antwort verarbeiten
-                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult(intent, REQUEST_CONTACTPICKER);
-                Log.d(LOG_CALLER, "Btn pressed");
+                Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(i, REQUEST_CONTACTPICKER);
             }
-        };
-        btnContact.setOnClickListener(btnCL);
+        });
         return v;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == REQUEST_CONTACTPICKER && resultCode == Activity.RESULT_OK)
+        {
+            // Auslesen der ContactID
+            Uri contactUri = data.getData();
+            Cursor cursor = getActivity().getContentResolver().query(contactUri, null, null, null, null);
+            cursor.moveToFirst();
+            String contactID = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+
+            // Neues Kontakt-Objekt erstellen und ID festlegen
+            Contact contact = new Contact();
+            contact.setId(Integer.parseInt(contactID));
+
+            // Namen des Kontaktes auslesen
+            String conName = readName(contactUri);
+            contact.setName(conName);
+
+            // Telefonnummer des Kontaktes auslesen
+            String conNumber = readNumber(contactUri);
+            contact.setNumber(conNumber);
+
+            // TODO Abfrage ob der Kontakt hinzugefügt werden soll
+            //        Log.d(LOG_CALLER, "Name: " + conName + " - Number: " + conNumber + " - Birthday: " + conBirthD);
+        }
+    }
+
+
+    private String readName(Uri contactUri)
+    {
+        Cursor cursor = getActivity().getContentResolver().query(contactUri, null, null, null, null);
+        cursor.moveToFirst();
+        int column = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        String temp = cursor.getString(column);
+        cursor.close();
+        return temp;
+    }
+
+    private String readNumber(Uri contactUri)
+    {
+        Cursor cursor = getActivity().getContentResolver().query(contactUri, null, null, null, null);
+        cursor.moveToFirst();
+        int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+        String temp = cursor.getString(column);
+        cursor.close();
+        return temp;
+    }
+
 
 }
 
