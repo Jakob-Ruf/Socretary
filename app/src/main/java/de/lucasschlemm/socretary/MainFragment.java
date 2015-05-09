f409d30950c7cb282380a654d35b96732487d9a1
Lucas_Schlemm <Lucas@Schlemms.de>
2015-05-09 15:14:11 +0200

    Fotos werden ausgelesen und angezeigt. Ist ein Bild in voller Größe vorhanden, wird es genutzt. Sonst gibt es das Thumbnail.

--- app/src/main/java/de/lucasschlemm/socretary/MainFragment.java
+++ app/src/main/java/de/lucasschlemm/socretary/MainFragment.java
@@ -1,8 +1,12 @@
 package de.lucasschlemm.socretary;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -12,10 +16,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by lucas.schlemm on 04.03.2015.
@@ -29,6 +34,7 @@ public class MainFragment extends Fragment
 	private final static int REQUEST_CONTACTPICKER = 1;
 
 	private ArrayList<Contact> contacts;
+	private ImageView          imageView;
 
 	@Nullable
 	@Override
@@ -36,6 +42,8 @@ public class MainFragment extends Fragment
 	                         Bundle savedInstanceState)
 	{
 		View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+		imageView = (ImageView) v.findViewById(R.id.img_example);
 
 		TextView txt     = (TextView) v.findViewById(R.id.txt_example);
 		String   inhalt  = "Lucas ";
@@ -86,6 +94,8 @@ public class MainFragment extends Fragment
 			// Adresse auslesen
 			String[] conAdress = readAdress(contactID);
 
+			Bitmap conPic = openDisplayPhoto(Long.valueOf(contactID));
+
 			boolean readAll = false;
 			if (readAll)
 			{
@@ -101,9 +111,11 @@ public class MainFragment extends Fragment
 
 
 			String lastContact = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.LAST_TIME_CONTACTED));
-			Long temp = (Long) Long.valueOf(lastContact);
-			Date tempDate = new Date(temp);
-			Log.v(LOG_CALLER, LOG_CALLER + " letzter Kontakt:- " + tempDate.toString());
+			Long temp = (Long) Long.valueOf(contactID);
+			//Date tempDate = new Date(temp);
+			//Log.v(LOG_CALLER, LOG_CALLER + " letzter Kontakt:- " + tempDate.toString());
+
+			cursor.close();
 
 
 			Contact contact = new Contact();
@@ -113,12 +125,17 @@ public class MainFragment extends Fragment
 			contact.setBirthday(conBDay);
 			contact.setLocationHome(conAdress);
 
-			contacts.add(contact);
+			//TODO @Lucas: Kontaktbild speichern oder dynamisches Laden??
 
+
+			// Ausgabe der ausgewählte Kontakte
+			contacts.add(contact);
 			for (Contact a : contacts)
 			{
 				Log.d(LOG_CALLER, "OnResult: Name " + a.getName() + " - Nummer " + a.getNumber() + " - Geburtstag " + a.getBirthday() + " - Wohnort " + a.getLocationHomeComplete());
 			}
+
+			imageView.setImageBitmap(openDisplayPhoto(temp));
 
             /*
 
@@ -126,6 +143,8 @@ public class MainFragment extends Fragment
             Log.d(LOG_CALLER, "Name: " + conName + " - Number: " + conNumber);
            // sendText(conNumber, conName);
 
+
+			// Auslösen einer Benachrichtigung
             Intent intent = new Intent();
             intent.setAction("de.lucasschlemm.CUSTOM_INTENT");
             intent.putExtra("type", "text");
@@ -134,6 +153,56 @@ public class MainFragment extends Fragment
 
 		}
 	}
+
+	//Liest das volle Bild
+	public Bitmap openDisplayPhoto(long contactId)
+	{
+		Uri contactUri      = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId);
+		Uri displayPhotoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.DISPLAY_PHOTO);
+		try
+		{
+			AssetFileDescriptor fd = getActivity().getContentResolver().openAssetFileDescriptor(displayPhotoUri, "r");
+			return BitmapFactory.decodeStream(fd.createInputStream());
+		} catch (IOException e)
+		{
+			Log.e(LOG_CALLER, "openDisplayPhoto : Foto nicht gefunden");
+			return readPicture(Long.toString(contactId));
+		}
+	}
+
+	//Liest Thumbnails
+	private Bitmap readPicture(String contactId)
+	{
+		Uri URI_PHOTO = ContactsContract.Data.CONTENT_URI;
+
+		String SELECTION_PHOTO = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+		String[] SELECTION_ARRAY_PHOTO = new String[]{
+				contactId,
+				ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE};
+
+		Cursor currPhoto  = getActivity().getContentResolver().query(URI_PHOTO, null, SELECTION_PHOTO, SELECTION_ARRAY_PHOTO, null);
+		int    indexPhoto = currPhoto.getColumnIndex(ContactsContract.CommonDataKinds.Photo.PHOTO);
+
+
+		if (currPhoto.getCount() > 0)
+		{
+			currPhoto.moveToFirst();
+			byte[] photoByte = currPhoto.getBlob(indexPhoto);
+			if (photoByte != null)
+			{
+				Bitmap bitmap = BitmapFactory.decodeByteArray(photoByte, 0, photoByte.length);
+				currPhoto.close();
+				return bitmap;
+			}
+		}
+		else
+		{
+			//TODO @Lucas: Abfrage falls kein Foto gefunden wurde.
+		}
+		currPhoto.close();
+		return null;
+	}
+
 
 	private String[] readAdress(String contactID)
 	{
