57db383ec392f4abd2912166a4f491124b2a40f9
Jakob Ruf <jakob.ruf93@gmail.com>
2015-05-27 15:03:51 +0200

    Utility-Methode für SHA1-Hashing

--- app/src/main/java/de/lucasschlemm/socretary/Utils.java
+++ app/src/main/java/de/lucasschlemm/socretary/Utils.java
@@ -22,6 +22,7 @@ import org.joda.time.DateTime;
 import org.joda.time.Days;
 
 import java.io.ByteArrayOutputStream;
+import java.security.MessageDigest;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -337,6 +338,31 @@ public class Utils
 		par.height = totalHeight + (listView.getDividerHeight() * (adapter.getCount() - 1));
 		listView.setLayoutParams(par);
 		listView.requestLayout();
+	}
+
+	public static String hashSHA1(String message){
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.update(message.getBytes("iso-8859-1"), 0, message.length());
+			byte[] sha1hash = md.digest();
+			return convertToHex(sha1hash);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static String convertToHex(byte[] sha1hash) {
+		StringBuilder buf = new StringBuilder();
+		for (byte b : sha1hash) {
+			int halfbyte = (b >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do {
+				buf.append((0 <= halfbyte) && (halfbyte <= 9) ? (char) ('0' + halfbyte) : (char) ('a' + (halfbyte - 10)));
+				halfbyte = b & 0x0F;
+			} while (two_halfs++ < 1);
+		}
+		return buf.toString();
 	}
 
 
